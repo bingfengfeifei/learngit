b492a6d01d0838dda560cf0385654a4a4c579a72
BingFengFeiFei <xszhengdelun@126.com>
2015-08-27 22:59:33 +0800

    添加<vector>和<string>头文件

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 int main()
 {
     cout << "hello world!" << endl;
     cout << "hello git" <<endl;//测试注释
+    
     return 0;
 }
